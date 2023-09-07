9eb723ff23dc4635000e7849bd6f1718b24215e5
miso10034 <123719113+miso10034@users.noreply.github.com>
2023-09-07 18:25:11 +0900

    Add files via upload

---
+++ 모던자바스크립트/ch4/arithmetic.js
@@ -0,0 +1,78 @@
+// var a = 3;
+// var b = 2;
+ 
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
+
+// console.log(10+20);
+// console.log('what the HELL' + ' HELP ME PLEASE O;');
+
+// console.log('긴급출동' + 119);
+// console.log('1' + 1);
+
+// console.log("1024" + 1);
+// console.log(+"1024" + 1);
+// console.log(true);
+// console.log(+true);
+// console.log(+false);
+
+// console.log('10' - 2);
+// console.log('10' * 2);
+// console.log('10' / 2);
+
+// console.log('10' - '2');
+// console.log('10' * '2');
+// console.log('10' / '2');
+
+// var a = 2;
+// a = a + 2;
+// console.log(a);
+
+// a = a - 2;
+// console.log(a);
+
+// a = a * 2;
+// console.log(a);
+
+// a = a / 2;
+// console.log(a);
+
+// var a = 3;
+// console.log(++a);
+// console.log(a++);
+// console.log(a);
+
+// var a = 10;
+// var b = 20;
+// var c = '20';
+
+// console.log(a>b);
+// console.log(a == b);
+// console.log(a != b);
+// console.log(a < b);
+// console.log(b == c);
+// console.log(b === c);
+
+// console.log('ABCD' > 'ABDC');
+
+// var size1 = 1024;
+// var size2 = '1024';
+
+// console.log(size1 == size2);
+// console.log(size1 === size2);
+
+// console.log(300 > '200');
+// console.log(true == '1');
+// console.log(true == '0');
+
+// console.log(null == undefined);
+// console.log(null == 0);
+// console.log(undefined == 0);
+
+var age = 30;
+var result = age >= 19 ? "성인입니다." : "미성년입니다."
+console.log(result);
