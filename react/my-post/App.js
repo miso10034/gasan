06e1230e67afd5469fcf00747ffc437119fe0f62
miso10034 <123719113+miso10034@users.noreply.github.com>
2023-09-22 17:49:18 +0900

    Add files via upload

---
+++ react/my-post/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import MyBoard from './MyBoard';
+
+function App() {
+  return (
+    <div className="App">
+      {/* 1. 여기에 직접 작성 가능
+          2. 따로 파일을 만들어서 불러오기
+      */}
+      <MyBoard></MyBoard>
+    </div>
+  );
+}
+
+
+export default App;
