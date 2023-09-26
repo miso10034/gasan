ba56500d7eed227e04e6f0df491dd3ae4f7e417f
miso10034 <123719113+miso10034@users.noreply.github.com>
2023-09-26 17:22:46 +0900

    Update App.js

--- my-shop/App.js
+++ my-shop/App.js
@@ -1,25 +1,41 @@
 import { Button, Navbar, Container, Nav} from 'react-bootstrap';
 import './App.css';
 import bgm from './img/bg2.jpg';
-import { useState } from 'react';
+import { useState } from 'react';// ./가 안붙어있는 애들 => 외부에서 제공되는 라이브러리
 import data from './data.js';
-import { Route, Routes, Link } from 'react-router-dom';
-import Detail from './detail';
-import About from './about';
+import { Route, Routes, Link, useNavigate, Outlet } from 'react-router-dom';
+// useNavigate => 페이지 이동 기능
+// Outlet => 위치 지정해서 출력해주는 기능
+import Detail from './pages/detail'; // ./로 시작하는 것들 => 내가 추가한 것들, js는 생략 가능
+import About from './pages/about';
 
 function App() {
   // object 자료형 [lps] => [{},{},{}] 형태로 되어 있음
   let [lps] = useState(data);
+  let navigate = useNavigate();
 
   return (
     <div className="App">
+
+      {/* <button  className="btn btn-primary" onClick={()=>{
+        navigate('/')
+      // navigate(-1) => 뒤로가기 
+      // navigate(1) => 앞으로 가기
+      }}>홈</button>
+      <button  className="btn btn-light" onClick={()=>{
+        navigate('/detail')
+      }}>상세페이지</button>
+      <button  className="btn btn-primary" onClick={()=>{
+        navigate('/about')
+      }}>더보기</button> */}
+
       <Navbar bg="light" data-bs-theme="light">
         <Container>
       <Navbar.Brand href="/">Record</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="/detail">Shop</Nav.Link>
+            <Nav.Link onClick={()=>{ navigate('/detail') }}>Shop</Nav.Link>
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/about">more</Nav.Link>
+            <Nav.Link onClick={()=>{ navigate('/about')}}>more</Nav.Link>
           </Nav>
         </Container>
       </Navbar><br></br>
@@ -49,6 +65,9 @@ function App() {
         }></Route>
         <Route path = "/detail" element = {<Detail></Detail>}></Route>
         <Route path = "/about" element = {<About></About>}></Route>
+        {/* 404 페이지 처리, * => 너가 가지고 있는 페이지를 제외하고는 모두 아래와 같은 표시를 해줄거야 */}
+        <Route path = "*" element = {<div>404 에러<br></br>없는 페이지 입니다.</div>}></Route>
+
       </Routes>
     </div>
   );
@@ -66,7 +85,7 @@ function Card(props){
     </div>
   )
 }
- 
 
 
 export default App;
+
